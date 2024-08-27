a8f48b5b1b389468adeb72936748e3a74a91a19a
gclhaha <gclhaha@qq.com>
2024-08-27 19:07:33 +0800

    theme color

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -32,11 +32,11 @@ const Button = (props: IButtonProps) => {
           }
 
           .btn-primary {
-            @apply text-white bg-primary-500;
+            @apply text-white bg-[#F33A4C];
           }
 
           .btn-primary:hover {
-            @apply bg-primary-600;
+            @apply bg-[#df2b3d];
           }
         `}
       </style>
